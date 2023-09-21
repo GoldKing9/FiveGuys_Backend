725b8984ca63bfdac151a18ebe2d07af52f53715
jsjune <avraskio@gmail.com>
2023-09-22 02:11:02 +0900

    [feat] 로그아웃 기능 구현 #6

--- webide/src/main/java/fiveguys/webide/api/user/controller/UserController.java
+++ webide/src/main/java/fiveguys/webide/api/user/controller/UserController.java
@@ -9,12 +9,16 @@ import fiveguys.webide.common.dto.ResponseDto;
 import fiveguys.webide.config.auth.LoginUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/auth")
 public class UserController {
     private final UserService userService;
@@ -33,6 +37,14 @@ public class UserController {
     @PostMapping("/reissue")
     public ResponseDto<LoginUserResponse> reissue(@Valid @RequestBody TokenRefreshRequest request) {
         return ResponseDto.success("토큰 재발행을 성공했습니다.", userService.reissue(request));
+    }
+
+    @PostMapping("/logout")
+    public ResponseDto<Void> logout(@AuthenticationPrincipal LoginUser loginUser) {
+        String nickname = loginUser.getUser().getNickname();
+        log.info("{} 로그아웃 시도", nickname);
+        SecurityContextHolder.clearContext();
+        return ResponseDto.success(nickname + " 로그아웃 성공", null);
     }
 
     @GetMapping("/test")
