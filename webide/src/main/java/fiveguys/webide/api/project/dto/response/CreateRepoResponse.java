fd16f3dfacc8a5af38eb2cf7538a541e3bed10ae
PanjunKwon <pndlm1979@naver.com>
2023-10-10 11:55:08 +0900

    [refactor] 레포 생성 api 응답데이터 추가 #34

--- webide/src/main/java/fiveguys/webide/api/project/dto/response/CreateRepoResponse.java
+++ webide/src/main/java/fiveguys/webide/api/project/dto/response/CreateRepoResponse.java
@@ -1,10 +1,16 @@
 package fiveguys.webide.api.project.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.type.descriptor.java.LocalDateJavaType;
 
-@AllArgsConstructor @Getter
+import java.time.LocalDateTime;
+
+@AllArgsConstructor @Getter @Builder
 public class CreateRepoResponse {
     private Long repoId;
     private String projectName;
+    private String createdAt;
+    private String updatedAt;
 }
